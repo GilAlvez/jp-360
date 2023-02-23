bb29ba3558ea9956f0e13214014f570e12b1e446
Gil Alves <gildson_af@outlook.com>
2023-02-22 22:03:25 -0300

    feat: Adds foward ref into Scene component

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,10 +1,11 @@
+import { forwardRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload } from '@react-three/drei'
 
-export default function Scene({ children, ...props }) {
+const Scene = forwardRef(function Scene({ children, ...props }, ref) {
   // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props}>
+    <Canvas {...props} ref={ref}>
       <directionalLight intensity={0.75} />
       <ambientLight intensity={0.75} />
       {children}
@@ -12,4 +13,6 @@ export default function Scene({ children, ...props }) {
       <OrbitControls />
     </Canvas>
   )
-}
+})
+
+export default Scene
