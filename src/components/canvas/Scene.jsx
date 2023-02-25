57e2ec9e5e6632f781a999e127e1ef0d4589d281
Gil Alves <gildson_af@outlook.com>
2023-02-24 21:31:32 -0300

    feat: Improves Scene controls and container

--- src/components/canvas/Scene.jsx
+++ src/components/canvas/Scene.jsx
@@ -1,17 +1,27 @@
-import { forwardRef } from 'react'
+import { Suspense, forwardRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload } from '@react-three/drei'
 
 const Scene = forwardRef(function Scene({ children, ...props }, ref) {
-  // Everything defined in here will persist between route changes, only children are swapped
   return (
-    <Canvas {...props} ref={ref}>
-      <directionalLight intensity={0.75} />
-      <ambientLight intensity={0.75} />
-      {children}
-      <Preload all />
-      <OrbitControls />
-    </Canvas>
+    <div className='absolute top-0 w-full -translate-x-1/2 left-1/2 md:w-4/5 bg-zinc-800 aspect-video'>
+      <Canvas ref={ref} {...props}>
+        <directionalLight intensity={0.75} />
+        <ambientLight intensity={0.75} />
+
+        <Suspense fallback={null}>{children}</Suspense>
+
+        <Preload all />
+        <OrbitControls
+          target={[0, 0, 0]}
+          enableZoom={false}
+          enablePan={false}
+          enableDamping={true}
+          rotateSpeed={0.5}
+          dampingFactor={0.05}
+        />
+      </Canvas>
+    </div>
   )
 })
 
