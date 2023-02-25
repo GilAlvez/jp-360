731ed8ec8b5dc5769318d0e41a67ce65baa4482f
Gil Alves <gildson_af@outlook.com>
2023-02-24 21:32:52 -0300

    feat: Improves Sky360 sizes and remove unused config

--- src/components/canvas/Sky360.jsx
+++ src/components/canvas/Sky360.jsx
@@ -1,14 +1,12 @@
 import { Sphere, useTexture } from '@react-three/drei'
 import { useRef } from 'react'
-import { BackSide, EquirectangularReflectionMapping } from 'three'
+import { BackSide } from 'three'
 
 export default function Sky360() {
   const sphereRef = useRef()
   const texture = useTexture('/360.jpg')
-  texture.mapping = EquirectangularReflectionMapping
-
   return (
-    <Sphere args={[50, 64, 32]} ref={sphereRef}>
+    <Sphere args={[500, 60, 40]} ref={sphereRef}>
       <meshBasicMaterial map={texture} side={BackSide} />
     </Sphere>
   )
