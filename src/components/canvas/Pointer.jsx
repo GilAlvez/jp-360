8cc8a33ca3552a6407025983e8700b93439b436b
Gil Alves <gildson_af@outlook.com>
2023-02-27 21:48:37 -0300

    feat: Improves Pointer component position with angle calc

--- src/components/canvas/Pointer.jsx
+++ src/components/canvas/Pointer.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { useCursor, meshPhysicalMaterial } from '@react-three/drei'
+import { useCursor } from '@react-three/drei'
 
-export default function Pointer({ route, ...props }) {
+export default function Pointer({ route, hAngle = 0, vAngle = 90, radius = 500, ...props }) {
   const router = useRouter()
   const [hovered, hover] = useState(false)
-
   useCursor(hovered)
+
+  const x = radius * Math.sin((vAngle * Math.PI) / 180) * Math.cos((hAngle * Math.PI) / 180)
+  const y = radius * Math.cos((vAngle * Math.PI) / 180)
+  const z = radius * Math.sin((vAngle * Math.PI) / 180) * Math.sin((hAngle * Math.PI) / 180)
 
   return (
     <mesh
       onClick={() => router.push(route)}
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}
+      position={[x, y, z]}
       {...props}>
-      <sphereGeometry args={[0.5, 64, 64]} />
-      <meshPhysicalMaterial roughness={1} color={hovered ? '#555' : '#fff'} />
+      <sphereGeometry args={[10, 64, 64]} />
+      <meshPhysicalMaterial roughness={1} color={hovered ? '#999' : '#fff'} />
     </mesh>
   )
 }
