820a6fcef6ee824049907390b69aa33524f8ac71
Gil Alves <gildson_af@outlook.com>
2023-02-28 20:43:11 -0300

    feat: Adds ...rest props

--- src/components/canvas/Sky360.jsx
+++ src/components/canvas/Sky360.jsx
@@ -1,12 +1,13 @@
 import { Sphere, useTexture } from '@react-three/drei'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { BackSide } from 'three'
 
-export default function Sky360({ imageUrl }) {
+export default function Sky360({ imageUrl, ...rest }) {
   const sphereRef = useRef()
   const texture = useTexture(imageUrl)
+
   return (
-    <Sphere args={[500, 60, 40]} ref={sphereRef}>
+    <Sphere args={[500, 60, 40]} ref={sphereRef} {...rest}>
       <meshBasicMaterial map={texture} side={BackSide} />
     </Sphere>
   )
--- src/components/dom/Instructions.jsx
+++ src/components/dom/Instructions.jsx
@@ -1,6 +1,8 @@
-export default function Instructions({ children }) {
+export default function Instructions({ children, ...rest }) {
   return (
-    <div className='w-5/6 max-w-lg px-10 py-8 m-8 mx-auto text-sm rounded-lg shadow-xl bg-zinc-800 md:text-base'>
+    <div
+      className='w-5/6 max-w-lg px-10 py-8 m-8 mx-auto text-sm rounded-lg shadow-xl bg-zinc-800 md:text-base'
+      {...rest}>
       <p>{children}</p>
     </div>
   )
