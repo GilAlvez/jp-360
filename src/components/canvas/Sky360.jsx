c30186076d84e9ef99e0fed1a43957293a8def32
Gil Alves <gildson_af@outlook.com>
2023-02-27 22:20:27 -0300

    feat: Adds Sky360 prop dynamic image url

--- src/components/canvas/Sky360.jsx
+++ src/components/canvas/Sky360.jsx
@@ -2,9 +2,9 @@ import { Sphere, useTexture } from '@react-three/drei'
 import { useRef } from 'react'
 import { BackSide } from 'three'
 
-export default function Sky360() {
+export default function Sky360({ imageUrl }) {
   const sphereRef = useRef()
-  const texture = useTexture('/360.jpg')
+  const texture = useTexture(imageUrl)
   return (
     <Sphere args={[500, 60, 40]} ref={sphereRef}>
       <meshBasicMaterial map={texture} side={BackSide} />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,15 +6,17 @@ const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: true })
 const Pointer = dynamic(() => import('@/components/canvas/Pointer'), { ssr: false })
 const Sky360 = dynamic(() => import('@/components/canvas/Sky360'), { ssr: false })
 
-export default function Page(props) {
+export default function Page() {
   return (
     <>
       <Seo />
+
       <Scene>
-        <Sky360 />
+        <Sky360 imageUrl={'/360.jpg'} />
         <Pointer hAngle={345} route='/janela' />
         <Pointer hAngle={305} route='/quarto' />
       </Scene>
+
       <Instructions>
         This project uses Nextjs + React-three-fiber and Threejs. Click on the{' '}
         <span className='font-bold text-cyan-200'>white pointer</span> to navigate to the{' '}
