7db30040c46301c791bec66ddb8a4950819687be
Gil Alves <gildson_af@outlook.com>
2023-03-20 21:14:56 -0300

    feat: Adds tailwind new configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   mode: 'jit',
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#fbf8f1',
+          100: '#f5eddf',
+          200: '#ead9be',
+          300: '#dcbe95',
+          400: '#d3a97c',
+          500: '#c2854d',
+          600: '#b47142',
+          700: '#965938',
+          800: '#794933',
+          900: '#623e2c',
+        },
+      },
+      screens: {
+        '3xl': '1920px',
+      },
+    },
   },
   plugins: [],
 }
