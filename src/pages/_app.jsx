8127e0215505c20d81f8dbe1681886f188acad1d
Gil Alves <gildson_af@outlook.com>
2023-03-20 21:17:02 -0300

    feat: Adds montserrat next font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,18 @@
+import Header from '@/components/dom/Header'
 import '@/styles/index.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Header />
       <Component {...pageProps} />
     </>
   )
