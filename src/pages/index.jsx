be7c675ac932f886de4fc8188bdbfd74e84ec26d
Gil Alves <gildson_af@outlook.com>
2023-03-20 21:18:00 -0300

    refact: Removes instructions and add Hero scene

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic'
-import Instructions from '@/components/dom/Instructions'
 import Seo from '@/components/dom/SEO'
 import Target from '@/components/canvas/Target'
+import Hero from '@/components/canvas/Hero'
 
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: true })
 const Pointer = dynamic(() => import('@/components/canvas/Pointer'), { ssr: false })
@@ -12,17 +12,15 @@ export default function Page() {
     <>
       <Seo />
 
-      <Scene preloadImage='/360-thumb.jpg'>
+      <Hero preloadImage='/360-thumb.jpg'>
+        <Sky360 imageUrl='/3603.jpg' />
+      </Hero>
+
+      {/* <Scene preloadImage='/360-thumb.jpg'>
         <Sky360 imageUrl='/360.jpg' />
         <Pointer hAngle={345} route='/#test' />
         <Target hAngle={305} route='/quarto' />
-      </Scene>
-
-      <Instructions id='test'>
-        This project uses Nextjs + React-three-fiber and Threejs. Click on the{' '}
-        <span className='font-bold text-cyan-200'>white pointer</span> to navigate to the{' '}
-        <span className='font-bold text-green-200'>/[dynamic-route]</span> page.
-      </Instructions>
+      </Scene> */}
     </>
   )
 }
