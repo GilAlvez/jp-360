8497909f363857fc9d69fb1bd36b27c7fc983973
Gil Alves <gildson_af@outlook.com>
2023-03-22 19:21:49 -0300

    feat: Adds new canvas loading to Hero component

--- src/components/canvas/Environments/Hero.jsx
+++ src/components/canvas/Environments/Hero.jsx
@@ -1,17 +1,15 @@
-import { Html, OrbitControls, useProgress } from '@react-three/drei'
+import CanvasLoading from '@/components/dom/CanvasLoading'
+import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense, forwardRef } from 'react'
-
-function Loader() {
-  const { progress } = useProgress()
-  return <Html center>{progress} % loaded</Html>
-}
 
 const Hero = forwardRef(function Hero({ children, preloadImage, ...props }, ref) {
   return (
     <div className='w-full relative aspect-square md:aspect-[21_/_10] 3xl:aspect-[21_/_9]'>
+      <CanvasLoading src={preloadImage} />
+
       <Canvas ref={ref} {...props}>
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={null}>
           <directionalLight intensity={0.75} />
           <ambientLight intensity={0.75} />
 
