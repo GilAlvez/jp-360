e4d04f2ac02bddbcf6de45e315f8deab58f9097f
Gil Alves <gildson_af@outlook.com>
2023-03-23 17:42:25 -0300

    feat: Removes layout responsabilty from Hero component

--- src/components/canvas/Environments/Hero.jsx
+++ src/components/canvas/Environments/Hero.jsx
@@ -1,31 +1,26 @@
-import CanvasLoading from '@/components/dom/CanvasLoading'
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Suspense, forwardRef } from 'react'
 
 const Hero = forwardRef(function Hero({ children, preloadImage, ...props }, ref) {
   return (
-    <div className='w-full relative aspect-square md:aspect-[21_/_10] 3xl:aspect-[21_/_9]'>
-      <CanvasLoading src={preloadImage} />
+    <Canvas ref={ref} {...props}>
+      <Suspense fallback={null}>
+        <directionalLight intensity={0.75} />
+        <ambientLight intensity={0.75} />
 
-      <Canvas ref={ref} {...props}>
-        <Suspense fallback={null}>
-          <directionalLight intensity={0.75} />
-          <ambientLight intensity={0.75} />
+        {children}
 
-          {children}
-
-          <OrbitControls
-            target={[0, 0, 0]}
-            enableZoom={false}
-            enablePan={false}
-            enableRotate={false}
-            autoRotate={true}
-            autoRotateSpeed={0.25}
-          />
-        </Suspense>
-      </Canvas>
-    </div>
+        <OrbitControls
+          target={[0, 0, 0]}
+          enableZoom={false}
+          enablePan={false}
+          enableRotate={false}
+          autoRotate={true}
+          autoRotateSpeed={0.25}
+        />
+      </Suspense>
+    </Canvas>
   )
 })
 
