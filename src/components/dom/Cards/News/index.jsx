6b584a063d4d352f1e0a23ec639c5973cac5484a
Gil Alves <gildson_af@outlook.com>
2023-03-24 15:23:52 -0300

    feat: Adds News Card component

---
+++ src/components/dom/Cards/News/index.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function NewsCard({ title, subtitle, link, imageUrl }) {
+  return (
+    <div className='overflow-hidden transition-shadow duration-300 ease-in-out bg-white rounded-lg shadow-lg hover:shadow-xl'>
+      <figure className='relative w-full h-32 '>
+        <Image src={imageUrl} alt={title} className='object-cover' fill />
+      </figure>
+
+      <div className='flex flex-col gap-2 p-6'>
+        <h5>{title}</h5>
+        <p className='text-sm line-clamp-2 text-stone-700'>{subtitle}</p>
+        <a href={link} className='self-end mt-2 btn btn-primary btn-sm btn-text'>
+          Leia Mais
+        </a>
+      </div>
+    </div>
+  )
+}
