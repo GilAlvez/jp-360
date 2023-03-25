e7f6983e0f0252b0b5fd1b1ab45489e81de2ed81
Gil Alves <gildson_af@outlook.com>
2023-03-25 16:45:07 -0300

    feat: Improves creator card experience

--- src/components/dom/Cards/Creators/index.jsx
+++ src/components/dom/Cards/Creators/index.jsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 export const CreatorCard = ({ name, role, bio, imageSrc, socialLinks }) => {
   return (
     <div className='flex items-center w-full'>
-      <figure className='relative self-stretch w-1/3'>
+      <figure className='relative self-stretch w-1/3 min-w-[160px]'>
         <Image src={imageSrc} alt={`Foto de ${name}`} className='object-cover rounded' fill />
       </figure>
       <div className='w-2/3 p-4'>
         <h4 className='text-stone-800'>{name}</h4>
-        <h6 className='mb-2 text-lg font-medium text-stone-400'>{role}</h6>
-        <p className='text-stone-700 line-clamp-4'>{bio}</p>
-        <ul className='flex mt-4'>
+        <h6 className='mb-1 text-lg font-normal text-stone-500'>{role}</h6>
+        <p className='text-sm text-stone-700 line-clamp-4'>{bio}</p>
+        <ul className='flex mt-3'>
           {socialLinks.map((link) => {
             const Icon = link.icon
             return (
