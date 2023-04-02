d23509a9af75124d2e1abc96255142e3469d14e9
Gil Alves <gildson_af@outlook.com>
2023-04-02 16:35:22 -0300

    feat: Adds a new Header conditional float

--- src/components/dom/Header/index.jsx
+++ src/components/dom/Header/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import HeaderDesktop from './desktop'
 import { HeaderMobile } from './mobile'
 
-const Header = ({ hidden = false }) => {
+const Header = ({ hidden = false, float = false }) => {
   const [scrollPosition, setScrollPosition] = useState(0)
 
   useEffect(() => {
@@ -17,9 +17,10 @@ const Header = ({ hidden = false }) => {
     <>
       <div className={`sticky top-0 z-50 ${hidden && 'hidden'}`}>
         <header
-          className={`app-container absolute inset-0 flex items-center justify-between h-16 gap-12 text-white backdrop-blur-[1px] transition-all duration-300 ${
-            scrollPosition > 0 ? 'bg-stone-800 ' : 'bg-black/50'
-          }`}>
+          className={`app-container  flex items-center justify-between h-16 gap-12 text-white backdrop-blur-[1px] transition-all duration-300 
+            ${scrollPosition > 0 ? 'bg-stone-900' : 'bg-black/50'}
+            ${float ? 'absolute inset-0' : 'bg-stone-900'} 
+          `}>
           <HeaderDesktop />
           <HeaderMobile />
         </header>
