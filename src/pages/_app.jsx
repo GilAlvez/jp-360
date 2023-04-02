d2b4409eea0666b2c6a0f75cd93df35e71045152
Gil Alves <gildson_af@outlook.com>
2023-04-02 16:34:24 -0300

    feat: Adds a container layout style

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,6 @@
 import { Montserrat } from 'next/font/google'
 
 import Footer from '@/components/dom/Footer'
-import { columns } from '@/components/dom/Footer/columns'
 import Header from '@/components/dom/Header'
 import '@/styles/index.scss'
 import { useRouter } from 'next/router'
@@ -19,18 +18,11 @@ export default function App({ Component, pageProps }) {
         }
       `}</style>
 
-      <Header hidden={pathname === '/404' && '/500'} />
-      <Component {...pageProps} />
-      <Footer
-        hidden={pathname === '/404' && '/500'}
-        shortand={false}
-        logoUrl='/LogoWhite.svg'
-        description='Lorem ipsum dolor sit amet, consectetur adip lorem, sed diam lorem, sed diam nonum, lorem nonllicitud, lorem nonllicitud, lorem nonllicitud, lorem nonllicitud'
-        columns={columns}
-        socials={{ instagramUrl: 'https://www.instagram.com/' }}
-        legals={{}}
-        copyright='Copyright © 2023 '
-      />
+      <div className='flex flex-col min-h-screen'>
+        <Header hidden={pathname === '/404' && '/500'} float={pathname === '/'} />
+        <Component {...pageProps} />
+        <Footer hidden={pathname === '/404' && '/500'} short={pathname !== '/'} />
+      </div>
     </>
   )
 }
