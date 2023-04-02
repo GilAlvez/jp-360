31b1790938936ddace529a6f94559c7d4fdd2765
Gil Alves <gildson_af@outlook.com>
2023-04-02 16:33:59 -0300

    refact: Simplifies Footer component

--- src/components/dom/Footer/index.jsx
+++ src/components/dom/Footer/index.jsx
@@ -1,71 +1,68 @@
-import Image from 'next/image'
-import { Legals } from './legals'
+import { InstagramLogo } from 'phosphor-react'
 
-import { Socials } from './socials'
+export default function Footer({ hidden = false, short = false }) {
+  const columns = [
+    {
+      title: 'Contato',
+      data: [
+        { name: 'contato@gmail.com', link: 'mailto:contato@gmail.com' },
+        { name: '(83) 9 9999-9999', link: 'tel:+5583999999999p000' },
+      ],
+    },
+  ]
 
-const Footer = ({ columns, logoUrl, description, legals, copyright, socials, shortand, hidden = false, ...rest }) => {
+  const socials = [{ icon: InstagramLogo, link: 'link' }]
+
   return (
-    <>
-      {shortand ? (
-        copyright && (
-          <footer className={`py-3 text-center app-container text-stone-400 bg-stone-900 ${hidden && 'hidden'}`}>
-            {copyright}
-          </footer>
-        )
-      ) : (
-        <>
-          <footer
-            className={`flex flex-col gap-4 pt-10 pb-4 mt-auto app-container bg-stone-800 ${hidden && 'hidden'}`}
-            {...rest}>
-            <div className='flex flex-col gap-4 lg:gap-8 lg:flex-row'>
-              <div className='flex flex-col gap-4 text-gray-100 md:w-full'>
-                {logoUrl && (
-                  <figure className='relative w-40 h-12'>
-                    <Image src={logoUrl} alt='Logo' fill />
-                  </figure>
-                )}
-                <p className='text-base'>{description}</p>
-              </div>
-              {columns && (
-                <table className='w-full text-gray-100 border-separate table-fixed lg:table-auto lg:pt-14'>
-                  <thead>
-                    <tr className='text-left'>
-                      {columns?.map(({ title }) => (
-                        <th className='py-1 font-medium text-gray-500' key={title}>
-                          {title}
-                        </th>
-                      ))}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className='text-left'>
-                      {columns?.map(({ content }, index) => (
-                        <td className='pr-4 align-top last-of-type:pr-0' key={index}>
-                          <div className='flex flex-col gap-2'>{content}</div>
-                        </td>
-                      ))}
-                    </tr>
-                  </tbody>
-                </table>
-              )}
+    <div className='mt-auto'>
+      {!short && (
+        <footer className={`py-8 app-container d-footer bg-stone-800 text-stone-100 ${hidden && 'hidden'}`}>
+          <div>
+            <svg
+              width='50'
+              height='50'
+              viewBox='0 0 24 24'
+              xmlns='http://www.w3.org/2000/svg'
+              fill-rule='evenodd'
+              clip-rule='evenodd'
+              class='fill-current'>
+              <path d='M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z'></path>
+            </svg>
+            <h5>Nome do Projeto.</h5>
+          </div>
+          {columns.map((col) => (
+            <div key={col.title}>
+              <h6 className='d-footer-title'>{col.title}</h6>
+              {col.data.map((cell) => (
+                <a key={cell.name} className='d-link d-link-hover' href={cell.link}>
+                  {cell.name}
+                </a>
+              ))}
             </div>
-
-            <div className='flex flex-col justify-between gap-4 lg:flex-row'>
-              <Socials socials={socials} />
-              <Legals legals={legals} />
-            </div>
-          </footer>
-
-          {copyright && (
-            <footer
-              className={`py-3 text-sm text-center app-container text-stone-400 bg-stone-900 ${hidden && 'hidden'}`}>
-              {copyright}
-            </footer>
-          )}
-        </>
+          ))}
+        </footer>
       )}
-    </>
+
+      <footer className='items-center py-4 gap-y-4 d-footer bg-stone-900 text-stone-300 app-container'>
+        <div className='items-center grid-flow-col text-center'>
+          <p>Copyright © 2023 - Todos os direitos reservados</p>
+        </div>
+        <div className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
+          {socials.map((social) => {
+            const Icon = social.icon
+            return (
+              <a
+                key={social.link}
+                href={social.link}
+                target='_blank'
+                rel='noreferrer noopener'
+                className='fill-current'>
+                <Icon className='text-2xl hover:text-primary-900' />
+              </a>
+            )
+          })}
+        </div>
+      </footer>
+    </div>
   )
 }
-
-export default Footer
