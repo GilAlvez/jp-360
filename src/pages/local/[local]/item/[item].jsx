6ffa7c48f770e44916dc8756aaeca02090c10691
Gil Alves <gildson_af@outlook.com>
2023-04-08 13:11:28 -0300

    feat: Adds new model canvas components and specific table

--- src/pages/local/[local]/item/[item].jsx
+++ src/pages/local/[local]/item/[item].jsx
@@ -1,17 +1,112 @@
 import Seo from '@/components/dom/Seo'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 
-// const Scene = dynamic(() => import('@/components/canvas/Environments/Scene'), { ssr: true })
-// const Sky360 = dynamic(() => import('@/components/canvas/Environments/Sky360'), { ssr: true })
+const Model = dynamic(() => import('@/components/canvas/Environments/Model'), { ssr: true })
 // const Target = dynamic(() => import('@/components/canvas/Elements/Target'), { ssr: false })
 
 export default function ItemPage() {
   const route = useRouter()
+  const specifications = [
+    { title: 'Dimensão', value: '100 x 50 x 30 cm' },
+    { title: 'Peso', value: '2 kg' },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+    {
+      title: 'Notas',
+      value:
+        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+    },
+  ]
+
   return (
     <>
       <Seo />
 
-      <main className='app-container'>Página Dinamica do Item Clicado: {`{${route.query.item}}`}</main>
+      <div className='flex flex-grow h-[calc(100vh_-_10rem)] '>
+        {/* Página Dinamica do Item Clicado: {`{${route.query.item}}`} */}
+        <main id='local' className='relative w-2/3'>
+          <Model />
+        </main>
+
+        <section className='flex items-center flex-grow w-1/3 bg-white'>
+          <div className='max-h-full overflow-y-auto'>
+            <table className='min-w-full divide-y divide-stone-200'>
+              <tbody className='bg-white divide-y divide-stone-200'>
+                {specifications.map((spec, index) => (
+                  <tr key={index}>
+                    <td className='px-4 py-2 text-sm align-top text-stone-500'>{spec.title}</td>
+                    <td className='py-2 pr-4 text-sm text-stone-900'>{spec.value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
     </>
   )
 }
