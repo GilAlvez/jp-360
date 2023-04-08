d7530a809b47b370df078adadca0846f4b90d34c
Gil Alves <gildson_af@outlook.com>
2023-04-08 13:10:35 -0300

    refact: Removes Scene components dependecy from Sky360

--- src/components/canvas/Environments/Sky360.jsx
+++ src/components/canvas/Environments/Sky360.jsx
@@ -1,13 +1,35 @@
-import { Sphere, useTexture } from '@react-three/drei'
+import { OrbitControls, Sphere, useTexture } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import { useRef } from 'react'
 import { BackSide } from 'three'
 
-export default function Sky360({ imageUrl, ...rest }) {
+export default function Sky360({ imageUrl, children, ref, ...props }) {
+  return (
+    <Canvas ref={ref} {...props}>
+      <directionalLight intensity={0.75} />
+      <ambientLight intensity={0.75} />
+
+      <Sky imageUrl={imageUrl} />
+      {children}
+
+      <OrbitControls
+        target={[0, 0, 0]}
+        enableZoom={false}
+        enablePan={false}
+        enableDamping={true}
+        rotateSpeed={0.25}
+        dampingFactor={0.05}
+      />
+    </Canvas>
+  )
+}
+
+const Sky = ({ imageUrl, ...props }) => {
   const sphereRef = useRef()
   const texture = useTexture(imageUrl)
 
   return (
-    <Sphere args={[500, 60, 40]} ref={sphereRef} {...rest}>
+    <Sphere args={[500, 60, 40]} ref={sphereRef} {...props}>
       <meshBasicMaterial map={texture} side={BackSide} />
     </Sphere>
   )
--- src/pages/local/[local]/index.jsx
+++ src/pages/local/[local]/index.jsx
@@ -1,13 +1,12 @@
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
+
 import CanvasLoading from '@/components/dom/CanvasLoading'
 import { EnviromentCard } from '@/components/dom/Cards/Enviroments'
 import Seo from '@/components/dom/Seo'
 import { Text } from '@/components/dom/Text'
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/router'
 
-const Scene = dynamic(() => import('@/components/canvas/Environments/Scene'), { ssr: true })
 const Sky360 = dynamic(() => import('@/components/canvas/Environments/Sky360'), { ssr: true })
-
 const Target = dynamic(() => import('@/components/canvas/Elements/Target'), { ssr: false })
 
 export default function LocalPage() {
@@ -43,10 +42,10 @@ export default function LocalPage() {
         id='local'
         className='w-full relative aspect-[1_/_2] sm:aspect-square md:aspect-[21_/_10] 3xl:aspect-[21_/_9]'>
         <CanvasLoading src='/360-thumb.jpg' />
-        <Scene preloadImage='/360-thumb.jpg'>
-          <Sky360 imageUrl='/360.jpg' />
+
+        <Sky360 imageUrl='/360.jpg'>
           <Target hAngle={305} route={`/local/${route.query.local}/item/item-qualquer`} />
-        </Scene>
+        </Sky360>
       </main>
 
       <section className='flex flex-col lg:flex-row gap-7 py-14 app-container'>
