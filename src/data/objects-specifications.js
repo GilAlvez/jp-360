4449e3b8eab3b2726d1c2c26126e7d5a0683bc65
Gil Alves <gildson_af@outlook.com>
2023-04-15 17:10:18 -0300

    feat: Adds data layer to easier apply variants

---
+++ src/data/objects-specifications.js
@@ -0,0 +1,48 @@
+export const objectsSpecifications = [
+  {
+    slug: 'object-slug',
+    type: 'model',
+    url: '/old_wooden.glb',
+    specifications: [
+      { title: 'Nível de descrição', value: '100 x 50 x 30 cm' },
+      { title: 'Código de referência', value: '100 x 50 x 30 cm' },
+      { title: 'Título', value: '2 kg' },
+      { title: 'Datas', value: '2 kg' },
+      { title: 'Dimensão', value: '2 kg' },
+      { title: 'Suporte', value: '2 kg' },
+      { title: 'Localidade', value: '2 kg' },
+      { title: 'História custodial e arquivística', value: '2 kg' },
+      { title: 'Âmbito e conteúdo', value: '2 kg' },
+      { title: 'Palavra-chave', value: '2 kg' },
+      { title: 'Idioma e escrita', value: '2 kg' },
+      {
+        title: 'Notas',
+        value:
+          ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+      },
+    ],
+  },
+  {
+    slug: 'object-slug2',
+    type: 'image',
+    url: 'https://source.unsplash.com/random',
+    specifications: [
+      { title: 'Nível de descrição', value: '100 x 50 x 30 cm' },
+      { title: 'Código de referência', value: '100 x 50 x 30 cm' },
+      { title: 'Título', value: '2 kg' },
+      { title: 'Datas', value: '2 kg' },
+      { title: 'Dimensão', value: '2 kg' },
+      { title: 'Suporte', value: '2 kg' },
+      { title: 'Localidade', value: '2 kg' },
+      { title: 'História custodial e arquivística', value: '2 kg' },
+      { title: 'Âmbito e conteúdo', value: '2 kg' },
+      { title: 'Palavra-chave', value: '2 kg' },
+      { title: 'Idioma e escrita', value: '2 kg' },
+      {
+        title: 'Notas',
+        value:
+          ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
+      },
+    ],
+  },
+]
