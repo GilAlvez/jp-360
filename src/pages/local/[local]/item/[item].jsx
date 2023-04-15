d868190a866127978968d556d74e7ef2c4a5b7e6
Gil Alves <gildson_af@outlook.com>
2023-04-15 15:16:42 -0300

    feat: Adds new specifications on table

--- src/pages/local/[local]/item/[item].jsx
+++ src/pages/local/[local]/item/[item].jsx
@@ -8,73 +8,17 @@ const Datsun = dynamic(() => import('@/components/canvas/Models/Datsun'), { ssr:
 export default function ItemPage() {
   const route = useRouter()
   const specifications = [
-    { title: 'Dimensão', value: '100 x 50 x 30 cm' },
-    { title: 'Peso', value: '2 kg' },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
-    {
-      title: 'Notas',
-      value:
-        ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illo consectetur nulla quis hic rerum. Ducimus deleniti voluptates natus ipsa neque, suscipit distinctio doloribus pariatur quas impedit porro fugiat veritatis!',
-    },
+    { title: 'Nível de descrição', value: '100 x 50 x 30 cm' },
+    { title: 'Código de referência', value: '100 x 50 x 30 cm' },
+    { title: 'Título', value: '2 kg' },
+    { title: 'Datas', value: '2 kg' },
+    { title: 'Dimensão', value: '2 kg' },
+    { title: 'Suporte', value: '2 kg' },
+    { title: 'Localidade', value: '2 kg' },
+    { title: 'História custodial e arquivística', value: '2 kg' },
+    { title: 'Âmbito e conteúdo', value: '2 kg' },
+    { title: 'Palavra-chave', value: '2 kg' },
+    { title: 'Idioma e escrita', value: '2 kg' },
     {
       title: 'Notas',
       value:
