4243d445c90a1096233d7fda491811299eada823
Gil Alves <gildson_af@outlook.com>
2023-04-21 11:25:34 -0300

    feat: Adds link navigation to Header logo

--- src/components/dom/Header/desktop.jsx
+++ src/components/dom/Header/desktop.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import NavLinks from './nav'
 
 const HeaderDesktop = () => {
   return (
     <>
       <picture className='relative w-32 h-full '>
-        <Image src='/LogoWhite.svg' alt='Logo' fill priority />
+        <Link href='/#hero'>
+          <Image src='/LogoWhite.svg' alt='Logo' fill priority />
+        </Link>
       </picture>
       <NavLinks className='hidden md:block' />
     </>
